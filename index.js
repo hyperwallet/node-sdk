980f120230a3b0f55602037dadb3e3b34de86c8a
rjstanford <richard@richardstanford.com>
2016-04-07 14:43:54 -0500

    Version 0.0.2

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ module.exports.init = function(options) {
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
-	// Transfer Methods
+	// Prepaid Cards
 	////////////////////////////////////////////////////////////////////////////////
 
-	hyperwallet.createUserTransferMethod = function(userToken, data, callback) {
+	hyperwallet.createUserPrepaidCard = function(userToken, data, callback) {
 		if (!userToken) {
 			util.error({ message: 'User token is required' }, callback);
 			return;
@@ -50,37 +50,169 @@ module.exports.init = function(options) {
 		util.post('/users/' + encodeURIComponent(userToken) + '/transfer-methods', data, callback);
 	};
 
-	hyperwallet.getUserTransferMethod = function(userToken, transferMethodToken, callback) {
+	hyperwallet.getUserPrepaidCard = function(userToken, prepaidCardToken, callback) {
 		if (!userToken) {
 			util.error({ message: 'User token is required' }, callback);
 			return;
 		}
-		if (!transferMethodToken) {
-			util.error({ message: 'Transfer Method token is required' }, callback);
+		if (!prepaidCardToken) {
+			util.error({ message: 'Prepaid Card token is required' }, callback);
 			return;
 		}
-		util.get('/users/' + encodeURIComponent(userToken) + '/transfer-methods/' + encodeURIComponent(transferMethodToken), null, callback);
+		util.get('/users/' + encodeURIComponent(userToken) + '/prepaid-cards/' + encodeURIComponent(prepaidCardToken), null, callback);
 	};
 
-	hyperwallet.updateUserTransferMethod = function(userToken, transferMethodToken, data, callback) {
+	hyperwallet.updateUserPrepaidCard = function(userToken, prepaidCardToken, data, callback) {
 		if (!userToken) {
 			util.error({ message: 'User token is required' }, callback);
 			return;
 		}
-		if (!transferMethodToken) {
-			util.error({ message: 'Transfer Method token is required' }, callback);
+		if (!prepaidCardToken) {
+			util.error({ message: 'Prepaid Card token is required' }, callback);
 			return;
 		}
-		util.put('/users/' + encodeURIComponent(userToken) + '/transfer-methods/' + encodeURIComponent(transferMethodToken), data, callback);
+		util.put('/users/' + encodeURIComponent(userToken) + '/prepaid-cards/' + encodeURIComponent(prepaidCardToken), data, callback);
 	};
 
-	hyperwallet.listUserTransferMethods = function(userToken, options, callback) {
+	hyperwallet.listUserPrepaidCards = function(userToken, options, callback) {
 		if (!userToken) {
 			util.error({ message: 'User token is required' }, callback);
 			return;
 		}
-		util.get('/users/' + encodeURIComponent(userToken) + '/transfer-methods', options, callback);
+		util.get('/users/' + encodeURIComponent(userToken) + '/prepaid-cards', options, callback);
 	};
+
+	////////////////////////////////////////////////////////////////////////////////
+	// Bank Accounts
+	////////////////////////////////////////////////////////////////////////////////
+
+	hyperwallet.createUserBankAccount = function(userToken, data, callback) {
+		if (!userToken) {
+			util.error({ message: 'User token is required' }, callback);
+			return;
+		}
+		util.post('/users/' + encodeURIComponent(userToken) + '/bank-accounts', data, callback);
+	};
+
+	hyperwallet.getUserBankAccount = function(userToken, bankAccountToken, callback) {
+		if (!userToken) {
+			util.error({ message: 'User token is required' }, callback);
+			return;
+		}
+		if (!bankAccountToken) {
+			util.error({ message: 'Bank Account token is required' }, callback);
+			return;
+		}
+		util.get('/users/' + encodeURIComponent(userToken) + '/bank-accounts/' + encodeURIComponent(bankAccountToken), null, callback);
+	};
+
+	hyperwallet.updateUserBankAccount = function(userToken, bankAccountToken, data, callback) {
+		if (!userToken) {
+			util.error({ message: 'User token is required' }, callback);
+			return;
+		}
+		if (!bankAccountToken) {
+			util.error({ message: 'Bank Account token is required' }, callback);
+			return;
+		}
+		util.put('/users/' + encodeURIComponent(userToken) + '/bank-accounts/' + encodeURIComponent(bankAccountToken), data, callback);
+	};
+
+	hyperwallet.deactivateUserBankAccount = function(userToken, bankAccountToken, callback) {
+		if (!userToken) {
+			util.error({ message: 'User token is required' }, callback);
+			return;
+		}
+		if (!bankAccountToken) {
+			util.error({ message: 'Bank Account token is required' }, callback);
+			return;
+		}
+		var transition = {
+			"transition": "DE-ACTIVATED"
+		};
+		util.post('/users/' + encodeURIComponent(userToken) + '/bank-accounts/' + encodeURIComponent(bankAccountToken) + '/status-transitions', transition, callback);
+	};
+
+	hyperwallet.listUserBankAccounts = function(userToken, options, callback) {
+		if (!userToken) {
+			util.error({ message: 'User token is required' }, callback);
+			return;
+		}
+		util.get('/users/' + encodeURIComponent(userToken) + '/bank-accounts', options, callback);
+	};
+
+	////////////////////////////////////////////////////////////////////////////////
+	// Status Transitions
+	////////////////////////////////////////////////////////////////////////////////
+
+	hyperwallet.createPrepaidCardStatusTransition = function(userToken, cardToken, data, callback) {
+		if (!userToken) {
+			util.error({ message: 'User token is required' }, callback);
+			return;
+		}
+		if (!cardToken) {
+			util.error({ message: 'Card token is required' }, callback);
+			return;
+		}
+		util.post('/users/' + encodeURIComponent(userToken) + '/prepaid-cards/' + encodeURIComponent(cardToken) + '/status-transitions', data, callback);
+	};
+
+	hyperwallet.getPrepaidCardStatusTransition = function(userToken, prepaidCardToken, statusTransitionToken, callback) {
+		if (!userToken) {
+			util.error({ message: 'User token is required' }, callback);
+			return;
+		}
+		if (!prepaidCardToken) {
+			util.error({ message: 'Prepaid Card token is required' }, callback);
+			return;
+		}
+		if (!statusTransitionToken) {
+			util.error({ message: 'Status Transition token is required' }, callback);
+			return;
+		}
+		util.get('/users/' + encodeURIComponent(userToken) + '/prepaid-cards/' + encodeURIComponent(prepaidCardToken) + '/status-transitions/' + encodeURIComponent(statusTransitionToken), null, callback);
+	};
+
+	hyperwallet.listPrepaidCardStatusTransitions = function(userToken, prepaidCardToken, options, callback) {
+		if (!userToken) {
+			util.error({ message: 'User token is required' }, callback);
+			return;
+		}
+		if (!prepaidCardToken) {
+			util.error({ message: 'Prepaid Card token is required' }, callback);
+			return;
+		}
+		util.get('/users/' + encodeURIComponent(userToken) + '/prepaid-cards/' + encodeURIComponent(prepaidCardToken) + '/status-transitions' , options, callback);
+	};
+
+	hyperwallet.createBankAccountStatusTransition = function(userToken, cardToken, data, callback) {
+		if (!userToken) {
+			util.error({ message: 'User token is required' }, callback);
+			return;
+		}
+		if (!cardToken) {
+			util.error({ message: 'Bank Account token is required' }, callback);
+			return;
+		}
+		util.post('/users/' + encodeURIComponent(userToken) + '/bank-accounts/' + encodeURIComponent(cardToken) + '/status-transitions', data, callback);
+	};
+
+	hyperwallet.getBankAccountStatusTransition = function(userToken, bankAccountToken, statusTransitionToken, callback) {
+		if (!userToken) {
+			util.error({ message: 'User token is required' }, callback);
+			return;
+		}
+		if (!bankAccountToken) {
+			util.error({ message: 'Bank Account token is required' }, callback);
+			return;
+		}
+		if (!statusTransitionToken) {
+			util.error({ message: 'Status Transition token is required' }, callback);
+			return;
+		}
+		util.get('/users/' + encodeURIComponent(userToken) + '/bank-accounts/' + encodeURIComponent(bankAccountToken) + '/status-transitions/' + encodeURIComponent(statusTransitionToken), null, callback);
+	};
+
 
 	////////////////////////////////////////////////////////////////////////////////
 	// Payment Methods
