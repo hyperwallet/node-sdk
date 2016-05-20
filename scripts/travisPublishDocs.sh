dab1eaac2c7b7f84b16c060828516c9b660e6c29
Florian Krauthan <fkrauthan@hyperwallet.com>
2016-05-20 14:42:43 -0700

    Added manual github page deploy

--- scripts/travisPublishDocs.sh
+++ scripts/travisPublishDocs.sh
@@ -5,4 +5,18 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
 # Build and publish docs
-npm run deploy-docs
+npm run docs
+
+mkdir ../gh-pages
+cp -r docs/* ../gh-pages/
+cd ../gh-pages
+
+# Add branch
+git init
+git remote add origin https://${GH_TOKEN}@github.com/paypal/PayPal-node-SDK.git > /dev/null
+git checkout -B gh-pages
+
+# Push generated files
+git add .
+git commit -m "Documentation updated"
+git push origin gh-pages -fq > /dev/null
