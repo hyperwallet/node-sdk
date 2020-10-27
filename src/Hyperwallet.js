37aab7d49c6f7453ea3e6a892812dcb59aba3ecb
mgovindhasamy <mgovindhasamy@paypal.com>
2020-10-27 20:46:06 +0530

    remove unwanted lines

--- src/Hyperwallet.js
+++ src/Hyperwallet.js
@@ -1317,7 +1317,6 @@ export default class Hyperwallet {
     listPayments(options, callback) {
         this.client.doGet("payments", options, Hyperwallet.handle204Response(callback));
     }
-
     /**
      * Create a payment status transition
      *
